6b03c7a63b7cf6ec699ca495f40d8acc21cb17e9
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2018-08-25 16:08:15 +0300

    feat: add map

---
+++ src/scripts/index.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibGVuYWVtYXlhIiwiYSI6ImNpa3VhbXE5ZjAwMXB3eG00ajVyc2J6ZTIifQ.kmZ4yVcNrupl4H8EonM3aQ';
+
+new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/lenaemaya/cjl9fjvvh2d3y2rp72gz6zs2j'
+});
